fd82e2e74994970858a6e04b27d1ba4b9dd9db9c
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-24 16:37:04 -0800

    Separate

---
+++ HW3_main/Boxer.h
@@ -0,0 +1,6 @@
+#ifndef BOXER_H
+#define BOXER_H
+
+void box(std::string& type, int border);
+
+#endif BOXER_H
--- HW3_main/Source.cpp
+++ HW3_main/Source.cpp
@@ -5,42 +5,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Boxer.h"
 
 using std::cout;
 using std::cin;
 using std::endl;
-using std::vector;
 using std::string;
-
-void box(string& type, int border) {
-	//
-	if (border < 1) {
-		border = 1;
-	}
-	//top-bottom layer creater
-	for (int i = 0; i < 2 * border + 1; i++) {
-		//side layer creater
-		for (int j = 0; j < 2 + type.size() + 2 * border; j++) {
-			//placement of text
-			if (i == border) {
-				for (int k = 0; k < border; k++) {
-					cout << "*";
-
-				}
-				cout << " " << type << " ";
-				for (int k = 0; k < border; k++) {
-					cout << "*";
-				}
-				break; 
-			}
-			else {
-				cout << "*";
-			}
-		}
-		cout << endl;
-	}
-	return;
-}
 
 int main() {
 	string type;
