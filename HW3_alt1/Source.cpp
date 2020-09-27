7173cbe6a5168ef96a8d94bc28e8b4d21b48a3e9
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-26 19:38:28 -0800

    Start alt1

---
+++ HW3_alt1/Source.cpp
@@ -0,0 +1,9 @@
+// Source.cpp Hiromi Kageyama Sept. 26, 2020
+// Write a c++ program that follows the Collatz sequence
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
