aeee7962392d84271e78cf48aa8a82b4972978ed
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-26 19:49:21 -0800

    Add if

--- HW3_alt1/Source.cpp
+++ HW3_alt1/Source.cpp
@@ -8,10 +8,20 @@ using std::cin;
 using std::endl;
 
 void Collatz(int n) {
+	// if n != 1, continue running
 	while (n != 1) {
 		cout << n << " ";
 
+		// if n is odd
+		if (n & 1) {
+			n = n * 3 + 1;
+		}
+		// if n is even
+		else {
+			n = n / 2;
+		}
 	}
+
 	return;
 }
 
